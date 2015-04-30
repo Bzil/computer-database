ce1a49e86fbe855c27ebae50b612dac1781923a4
Bzil <bzchap@gmail.com>
2015-04-30 10:43:33 +0200

    Correct pom dependencies, add DateValidator documentation

--- cdb/binding/src/main/java/com/excilys/cdb/validation/DateValidator.java
+++ cdb/binding/src/main/java/com/excilys/cdb/validation/DateValidator.java
@@ -11,19 +11,39 @@ import org.springframework.context.i18n.LocaleContextHolder;
 
 import com.excilys.cdb.annotation.Date;
 
+/**
+ * The Class DateValidator.
+ */
 public class DateValidator implements ConstraintValidator<Date, String> {
 
+	/** Pattern to match all en / fr date */
 	private static final Pattern DATE_PATTERN = Pattern
 			.compile("^(\\d{2})-(\\d{2})-(\\d{4})$");
 
-	// 1 0 1 0 1 1 0 1 0 1 0 1 0
-	// D N O S A J J M A M F J
+	/**
+	 * Binary who represent months with 31 day <br />
+	 * current bit : 1 0 1 0 1 1 0 1 0 1 0 1 0 <br />
+	 * 31-d months : D N O S A J J M A M F J /
+	 */
 	private static final int MONTHS = 0x15AA;
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * javax.validation.ConstraintValidator#initialize(java.lang.annotation.
+	 * Annotation)
+	 */
 	@Override
 	public void initialize(Date constraintAnnotation) {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see javax.validation.ConstraintValidator#isValid(java.lang.Object,
+	 * javax.validation.ConstraintValidatorContext)
+	 */
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		if (value == null || value.trim().isEmpty()) {
@@ -46,6 +66,20 @@ public class DateValidator implements ConstraintValidator<Date, String> {
 
 	}
 
+	/**
+	 * Validate the current date so it matches mySQL format
+	 *
+	 * @See https://dev.mysql.com/worklog/task/?id=1872
+	 *      http://fr.wikipedia.org/wiki/Bug_de_l%27an_2038
+	 *
+	 * @param day
+	 *            the day
+	 * @param month
+	 *            the month
+	 * @param year
+	 *            the year
+	 * @return true, if successful
+	 */
 	protected boolean validate(int day, int month, int year) {
 		int maxDay = 31;
 
