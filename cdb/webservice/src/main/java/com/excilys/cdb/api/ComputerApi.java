58393d30da978dde2bd1d79205016529bbbd26b2
Bzil <bzchap@gmail.com>
2015-05-04 10:48:36 +0200

    Correct javadocapi

--- cdb/webservice/src/main/java/com/excilys/cdb/api/ComputerApi.java
+++ cdb/webservice/src/main/java/com/excilys/cdb/api/ComputerApi.java
@@ -41,20 +41,32 @@ public interface ComputerApi {
 	ResponseEntity<List<ComputerJson>> getAll();
 
 	/**
-	 * Creates the.
+	 * Creates the Computer with required parameter name, id = -1, companyId
+	 * Optional parameter are introduced, discontinued with pattern
+	 * <u>dd-mm-yyyy</u> and companyName. For example <i>{"id":"-1",
+	 * "name":"xyz", "companyId":"-1"}</i> will return
+	 * <i>{"id":XX,"name":"xyz","introduced"
+	 * :"","discontinued":"","companyId":-1,"companyName":""}</i> where XX is
+	 * the new id of the computer.
 	 *
 	 * @param dto
 	 *            the dto
-	 * @return the response entity
+	 * @return the response
 	 */
 	ResponseEntity<ComputerJson> create(ComputerJson dto);
 
 	/**
-	 * Update.
+	 * Update the Computer with required parameter name, id = XX, companyId
+	 * Optional parameter are introduced, discontinued with pattern
+	 * <u>dd-mm-yyyy</u> and companyName. For example <i>{"id":"XX",
+	 * "name":"xyz", "companyId":"-1"}</i> will return
+	 * <i>{"id":XX,"name":"xyz","introduced"
+	 * :"","discontinued":"","companyId":-1,"companyName":""}</i> where XX is
+	 * the id of the computer.
 	 *
 	 * @param dto
 	 *            the dto
-	 * @return the response entity
+	 * @return the response
 	 */
 	ResponseEntity<ComputerJson> update(ComputerJson dto);
 
